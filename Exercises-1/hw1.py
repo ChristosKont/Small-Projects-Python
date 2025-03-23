2e1da5c27811102acffcaa8fefd5ba602220cc4c
Christos Kontodimas <80399020+ChristosKont@users.noreply.github.com>
2025-03-23 19:59:08 +0200

    Add files via upload

---
+++ Exercises-1/hw1.py
@@ -0,0 +1,199 @@
+from operator import *
+
+
+#>>>>>>>>>>>>>>>>>>>>> Askhsh 1 <<<<<<<<<<<<<<<
+
+def sum_powers(x1, n1, x2, n2):
+    """ Ypologizei x1**n1+x2**n2
+
+    x1,n1,x2,n2 -- ari8moi
+    """
+
+    """ Xrhsimopoihste MONO ekfraseis klhshs,
+        OXI infix telestes (+,-,...) """
+
+    return __________________________
+
+
+
+#>>>>>>>>>>>>>>>>>>>>> Askhsh 2 <<<<<<<<<<<<<<<
+
+def my_abs(x):
+    """ Ypologizei thn apolyth timh tou x
+
+    x -- ari8mos
+
+    >>> my_abs(43.33)
+    43.33
+    >>> my_abs(-894.3)
+    894.3
+    """
+
+    """ Xrhsimopoihste MONO ekfraseis klhshs,
+        OXI infix telestes (+,-,...) """
+
+    return _______(__________,___________)
+
+
+#>>>>>>>>>>>>>>>>>>>>> Askhsh 3 <<<<<<<<<<<<<<<
+
+def func(s, n):
+    """Symplhrwste ta kena wste:
+
+    >>> func('hello',2)
+    'Hmmmm hello my hmm friend'
+    >>> func('goodbye',3)
+    'Hmmmmmm goodbye my hmmm friend'
+    """
+
+    return _________________________________
+
+
+
+#>>>>>>>>>>>>>>>>>>>>> Askhsh 4 <<<<<<<<<<<<<<<
+
+def func1(s):
+    """Symplhrwste ta kena wste na didetai to
+       akolou8o apotelesma
+
+    >>> func1('ni')
+    'Mini-Mini'
+    >>> func1('kros')
+    'Mikros-Mikros'
+    """
+
+    return ___________________
+
+
+#>>>>>>>>>>>>>>>>>>>>> Askhsh 5 <<<<<<<<<<<<<<<
+
+def func2():
+    """Symplhrwste ta kena wste na didetai to
+       akolou8o apotelesma
+
+    >>> func2()
+    Mini-Me-Mini-Me
+    """
+
+    return _________(func1(__________))
+
+
+#>>>>>>>>>>>>>>>>>>>>> Askhsh 6 <<<<<<<<<<<<<<<
+
+def func3(s):
+    """Symplhrwste ta kena wste na didetai to
+       akolou8o apotelesma
+
+    >>> func3(func3('tsouf'))
+    '((tsouf-tsouf)-(tsouf-tsouf))'
+    """
+
+    return ______________________
+    
+
+
+
+
+#>>>>>>>>>>>>>>>>>>>>> Askhsh 7 <<<<<<<<<<<<<<<
+
+def digit_sum(x):
+    """Ypologizei to a8roisma twn (dekadikwn) pshfiwn tou x
+
+    x -- 8etikos akeraios
+
+    >>> digit_sum(10000)
+    1
+    >>> digit_sum(615)
+    12
+    """
+
+    s = ________
+    while ________:
+        s = __________
+        x = __________
+
+    return ________
+
+
+
+#>>>>>>>>>>>>>>>>>>>>> Askhsh 8 <<<<<<<<<<<<<<<
+
+def keep_summing(x):
+    """Ypologizei to a8roisma twn pshfiwn synexws ews
+    otou to apotelesma exei ena mono pshfio
+
+    x -- 8etikos akeraios
+
+    >>> keep_summing(3)
+    3
+    >>> keep_summing(32)
+    5
+    >>> keep_summing(344)
+    2
+    >>> keep_summing(999)
+    9
+    >>> print(keep_summing(999))
+    9
+    """
+
+    """ GRAPSTE TON KWDIKA SAS APO KATW """
+
+
+
+
+#>>>>>>>>>>>>>>>>>>>>> Askhsh 9 <<<<<<<<<<<<<<<
+
+def draw_number(x):
+    """Emfanizei ton xarakthra | x fores
+
+    >>> draw_number(5)
+    |||||
+    >>> draw_number(0)
+    >>> draw_number(-3)
+    -|||
+    """
+
+    """ GRAPSTE TON KWDIKA SAS APO KATW """
+
+
+
+
+#>>>>>>>>>>>>>>>>>>>>> Askhsh 10 <<<<<<<<<<<<<<<
+
+def max_digit(x):
+    """Ypologizei to megalytero pshfio tou x
+
+    x -- 8etikos akeraios
+
+    >>> max_digit(45874543)
+    8
+    >>> max_digit(98287334) - max_digit(8)
+    1
+    """
+
+    """ GRAPSTE TON KWDIKA SAS APO KATW """
+
+
+
+
+
+#>>>>>>>>>>>>>>>>>>>>> Askhsh 11 <<<<<<<<<<<<<<<
+
+def count_digits(x, d):
+    """Ypologizei to plh8os twn pshfiwn iswn me d
+
+    x -- 8etikos akeraios
+    d -- akeraios apo 0 ews kai 9
+
+    >>> count_digits(1000, 0)
+    3
+    >>> count_digits(12944342, 2)
+    2
+    >>> count_digits(121,1)+ count_digits(121,2)
+    3
+    """
+
+    """ GRAPSTE TON KWDIKA SAS APO KATW """
+
+
+
---
+++ Exercises-3/hw3-sol/hw3.py
@@ -0,0 +1,215 @@
+
+#--------------------- Askisi 1 -----------------------------
+
+def pick_cherries_onebyone():
+    """Emfanizei string pou briskontai se fwliasmenes listes.
+
+    Prepei na exei to akolou8o apotelesma:
+
+    >>> pick_cherries_onebyone()
+    cherry1
+    cherry2
+    cherry3
+    cherry4
+    Yay!!!
+    """
+    cherry_field = ['cherry1', ['cherry2', ['cherry3', ['cherry4', ['Yay!!!', None]]]]]
+
+    print(cherry_field[0])
+    print(cherry_field[1][0])
+    print(cherry_field[1][1][0])
+    print(cherry_field[1][1][1][0])
+    print(cherry_field[1][1][1][1][0])
+
+
+    
+#--------------------- Askisi 2 -----------------------------
+
+def pick_cherries_iter(field):
+    """Emfanizei string pou briskontai se fwliasmenes listes.
+
+    field -- lista me fwliasmena string. Ka8e lista exei dyo stoixeia: to prwto einai
+             string kai to deutero einai eite lista ths idias morfhs 'h None. 
+             (Opws kai h cherry_field sto swma ths synarthshs pick_cherries_onebyone().
+
+    Paradeigmata:
+
+    >>> cherry_field = ['cherry1', ['cherry2', ['cherry3', ['cherry4', ['Yay!!!', None]]]]]
+    >>> pick_cherries_iter(cherry_field)
+    cherry1
+    cherry2
+    cherry3
+    cherry4
+    Yay!!!
+    >>> pick_cherries_iter(['Hello', ['world', None]])
+    Hello
+    world
+    """
+    _______________________
+    _______________________
+    _______________________
+    _______________________
+
+    while _________________
+        ___________________
+        print(____________)
+        ___________________
+
+
+#--------------------- Askisi 3 -----------------------------
+
+def pick_cherries_rec(field):
+    """Emfanizei string pou briskontai se fwliasmenes listes.
+
+    field -- lista me fwliasmena string. Ka8e lista exei dyo stoixeia: to prwto einai
+             string kai to deutero einai eite lista ths idias morfhs 'h None. 
+             (Opws kai h cherry_field sto swma ths synarthshs pick_cherries_onebyone().
+
+    Anadromiki synarthsh: den epitrepetai na xrhsimopoihsete entoles epanalhpshs
+    (for, while) 'h list comprehension.
+
+    Paradeigmata:
+
+    >>> cherry_field = ['cherry1', ['cherry2', ['cherry3', ['cherry4', ['Yay!!!', None]]]]]
+    >>> pick_cherries_rec(cherry_field)
+    cherry1
+    cherry2
+    cherry3
+    cherry4
+    Yay!!!
+    >>> pick_cherries_rec(['Hello', ['world', None]])
+    Hello
+    world
+    """
+    """ GRAPSTE TON KWDIKA SAS APO KATW. XRHSIMOPOIHSTE ANADROMH.
+    OXI FOR, WHILE, LIST COMPREHENSIONS."""
+
+    
+
+        
+#--------------------- Askisi 4 -----------------------------
+
+def pick_cherries_proc(field):
+    """Emfanizei string pou briskontai se fwliasmenes listes.
+
+    field -- lista me fwliasmena string. Ka8e lista exei dyo stoixeia: to prwto einai
+             string kai to deutero einai eite lista ths idias morfhs 'h None. 
+             (Opws kai h cherry_field sto swma ths synarthshs pick_cherries_onebyone().
+
+    Paradeigmata:
+
+    >>> cherry_field = ['cherry1', ['cherry2', ['cherry3', ['cherry4', ['Yay!!!', None]]]]]
+    >>> pick_cherries_proc(cherry_field)
+    cherry1
+    cherry2
+    cherry3
+    cherry4
+    Yay!!!
+    >>> pick_cherries_proc(['Hello', ['world', None]])
+    Hello
+    world
+    """
+    """ GRAPSTE TON KWDIKA SAS APO KATW. XRHSIMOPOIHSTE ANADROMH.
+    OXI FOR, WHILE, LIST COMPREHENSIONS."""
+
+    def flatten(ls):
+        _______________________
+        _______________________
+        _______________________
+        _______________________
+        
+
+    [print(__________) for _________ in flatten(___________)]
+        
+
+
+#--------------------- Askisi 5 -----------------------------
+
+def korakize(sentence):
+    """Metafrazei sta korakistika.
+
+    sentence -- string pou 8a metafrastei
+   
+    Epistrefei string metafrasmeno sta korakistika.
+
+    Paradeigmata:
+
+    >>> korakize('Kalimera')
+    'Kakalikamekaraka'
+    >>> korakize('Maria')
+    'Makarikaaka'
+    >>> korakize('Kalimera Maria')
+    'Kakalikamekaraka Makarikaaka'
+    >>> korakize('Na zi kanis i na min zi')
+    'Naka zika kakanikaska ika naka mikanka zika'
+    """
+    """GRAPSTE TON KWDIKA SAS APO KATW."""
+    i, l, = 0, len(sentence)
+    r, p =  0, 0
+    string = ''
+    
+    while (i < l):
+        if (sentence[i] != ' '):
+            if (r < 2):
+                string += sentence[i]
+                r += 1
+                i += 1
+            else:
+                string += "ka"
+                r = 0
+        else:
+            string += "ka "
+            i += 1
+            r = 0
+    string += "ka"
+
+    return string
+
+
+
+#--------------------- Askisi 6 -----------------------------
+
+def dekorakize(sentence):
+    """Metafrazei apo korakistika.
+
+    sentence -- string sta korakistika pou 8a metafrastei
+   
+    Epistrefei string. Einai h antistrofh synarthsh ths korakize.
+
+    Paradeigmata:
+
+    >>> dekorakize('Kakalikamekaraka')
+    'Kalimera'
+    >>> dekorakize('Makarikaaka')
+    'Maria'
+    >>> dekorakize('Kakalikamekaraka Makarikaaka')
+    'Kalimera Maria'
+    >>> dekorakize('Naka zika kakanikaska ika naka mikanka zika')
+    'Na zi kanis i na min zi'
+    """
+    """GRAPSTE TON KWDIKA SAS APO KATW."""
+    i, l = 0, len(sentence)
+    r, p = 0, 0
+    string = ''
+
+    while (i < l):
+        if (sentence[i] != ' '):
+            if (r < 2):
+                string += sentence[i]
+                r += 1
+                f = 0
+            else:
+                f += 1
+                if (f > 1):
+                    r = 0
+        else:
+            string += ' '
+        i += 1
+        
+    x = len(string)
+    if (l % 2 == 1):
+        string = string[:len(string)-1]
+        
+    return string
+
+
